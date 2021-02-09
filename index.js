efab057c7896011c9bd6a7822e082f9fe185c9bb
cdamyx <chris@amyx.engineering>
2021-02-08 22:12:30 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// const fs = require("fs");
+
+// fs.copyFileSync("file1.txt", "file2.txt");
+
+// var superheroes = require("superheroes");
+
+// var mySuperheroName = superheroes.random();
+
+// console.log(mySuperheroName);
+
+var villains = require("supervillains");
+
+var myName = villains.random();
+
+console.log(myName);
